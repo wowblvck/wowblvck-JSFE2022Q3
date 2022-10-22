e24c75ffdba39b0efa50f9dee50babb101fbe1b4
Indar Basto (@wowblvck) <indar.basto@gmail.com>
2022-10-22 19:20:19 +0300

    refactor: game.js

--- gem-puzzle/src/js/game.js
+++ gem-puzzle/src/js/game.js
@@ -135,4 +135,62 @@ export default class Game {
     }
     return res;
   }
+
+  static createPuzzle(n) {
+    const canvas = document.getElementById('puzzle');
+    canvas.width = 280;
+    canvas.height = 280;
+
+    const context = canvas.getContext('2d');
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    let cellSize = canvas.width / n;
+    let game = new Game(context, cellSize, n);
+    game.mix(300, n);
+    game.draw();
+
+    canvas.addEventListener('click', (e) => {
+      const x = (e.pageX - canvas.offsetLeft) / cellSize | 0;
+      const y = (e.pageY - canvas.offsetTop) / cellSize | 0;
+      this.onEvent(game, x, y);
+    });
+
+    canvas.addEventListener('touchend', (e) => {
+      const x = (e.touches[0].pageX - canvas.offsetLeft) / cellSize | 0;
+      const y = (e.touches[0].pageY - canvas.offsetTop) / cellSize | 0;
+      this.onEvent(game, x, y);
+    });
+
+    const list = document.querySelector('.frame__list');
+    list.addEventListener('change', (e) => {
+      e.preventDefault();
+      const nowstate = e.target.value;
+      const selection = document.querySelector('.frame__selection');
+      selection.textContent = `${nowstate}x${nowstate}`;
+
+      canvas.width = 280;
+      canvas.height = 280;
+
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      cellSize = canvas.width / e.target.value;
+      game = new Game(context, cellSize, e.target.value);
+      game.mix(300, e.target.value);
+      game.draw();
+      document.querySelector('.info__step').textContent = `${game.getClicks()}`;
+    });
+  }
+
+  static onEvent(game, x, y) {
+    game.move(x, y);
+    const canvas = document.getElementById('puzzle');
+    const context = canvas.getContext('2d');
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    game.draw();
+    document.querySelector('.info__step').textContent = `${game.getClicks()}`;
+    if (game.victory()) {
+      alert(`Собрано за ${game.getClicks()} касание!`);
+      game.mix(300);
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      game.draw(context, this.cellSize);
+    }
+  }
 }
