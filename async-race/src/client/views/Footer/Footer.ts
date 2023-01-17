c2059301b911c4f00122432c1b4b406c7850e964
Indar Basto <indar.basto@gmail.com>
2023-01-17 20:39:52 +0300

    fix: container margins

--- async-race/src/client/views/Footer/Footer.ts
+++ async-race/src/client/views/Footer/Footer.ts
@@ -5,7 +5,7 @@ class Footer implements AppComponent {
   render = () => `
     <footer class="footer mt-auto py-3">
       <div class="container">
-        <div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
           <div class="col-md-4 d-flex align-items-center">
             <p class="mb-3 mb-md-0 text-muted">&copy 2023 <a href="https://rs.school" target="_blank" class="text-muted">RS School</a></p>
           </div>
